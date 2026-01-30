391ee404c2bd50a61859736c6bd38309e50fd26a
Mohamed Elkenany <mohamed202202632@gmail.com>
2026-01-30 07:53:58 +0200

    style: customize Filament admin theme and layout views

---
+++ resources/views/filament/pages/analytics.blade.php
@@ -0,0 +1,11 @@
+<x-filament-panels::page>
+    <div class="space-y-6">
+        {{-- Header Widgets are automatically rendered by the class --}}
+        
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
+            {{-- Custom content can go here if needed --}}
+        </div>
+
+        {{-- Footer Widgets are automatically rendered by the class --}}
+    </div>
+</x-filament-panels::page>
